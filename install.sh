9db6d882a82b0c7c4e18aba5a2af5fd13525145e
Vlad Nikitin <51231679+Nightowlswing@users.noreply.github.com>
2021-10-08 16:19:07 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-pip3 install torch==1.8.1+cpu torchvision==0.9.1+cpu torchaudio==0.8.1 -f https://download.pytorch.org/whl/lts/1.8/torch_lts.html
+pip install torch==1.8.1+cpu torchvision==0.9.1+cpu torchaudio==0.8.1 -f https://download.pytorch.org/whl/lts/1.8/torch_lts.html
 apt install -y tesseract-ocr
 pip install opencv-python
 pip install pytesseract
